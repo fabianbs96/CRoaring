d6f3eef39f053e671ba98c6784e1c972d08e6fcf
fabianbs96 <fabianbs96@users.noreply.github.com>
2026-01-30 11:57:04 +0000

    deploy: 41100ea6a4f3e9e135d925333e8dc77e2c9f7ebf

---
+++ annotated_dup.js
@@ -0,0 +1,7 @@
+var annotated_dup =
+[
+    [ "roaring64_bulk_context_s", "df/dce/structroaring64__bulk__context__s.html", "df/dce/structroaring64__bulk__context__s" ],
+    [ "roaring_bitmap_s", "dd/d7a/structroaring__bitmap__s.html", "dd/d7a/structroaring__bitmap__s" ],
+    [ "roaring_bulk_context_s", "db/d0d/structroaring__bulk__context__s.html", "db/d0d/structroaring__bulk__context__s" ],
+    [ "roaring_uint32_iterator_s", "da/d27/structroaring__uint32__iterator__s.html", "da/d27/structroaring__uint32__iterator__s" ]
+];
---
+++ search/all_e.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['macos_0',['Usage (Using &lt;tt&gt;vcpkg&lt;/tt&gt; on Windows, Linux and macOS)',['../index.html#autotoc_md38',1,'']]],
+  ['macos_20visual_20studio_20or_20openharmony_20users_20should_20see_20below_1',['Building with cmake (Linux and macOS, Visual Studio or OpenHarmony users should see below)',['../index.html#autotoc_md35',1,'']]],
+  ['mailing_20list_20discussion_20group_2',['Mailing list/discussion group',['../index.html#autotoc_md53',1,'']]],
+  ['main_20api_20functions_3',['Main API functions',['../index.html#autotoc_md11',1,'']]],
+  ['main_20classes_4',['Main Classes',['../index.html#autotoc_md21',1,'']]],
+  ['memory_20allocators_5',['Custom memory allocators',['../index.html#autotoc_md30',1,'']]],
+  ['methods_2032_20bit_20and_2064_20bit_6',['Common Methods (32-bit and 64-bit)',['../index.html#autotoc_md22',1,'']]],
+  ['microbenchmarks_7',['Running microbenchmarks',['../index.html#autotoc_md29',1,'']]],
+  ['module_8',['Redis Module',['../index.html#autotoc_md51',1,'']]]
+];
